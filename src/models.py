609c9640fdcbab1b7d829e1672a005e90e2d3e24
Jake Cowton <jake.cowton@newcastle.ac.uk>
2015-11-19 16:27:11 +0000

    Model added

---
+++ src/models.py
@@ -0,0 +1,29 @@
+class Passenger(object):
+
+    p_id = None
+    survival = None
+    p_class = None
+    name = None
+    sex = None
+    age = None
+    n_sib_sp = None
+    n_pa_ch = None
+    ticket = None
+    fare = None
+    cabin = None
+    embarked = None
+
+    def __init__(p_id ,survival, p_class, name, sex, age, n_sib_sp, n_pa_ch,
+                 ticket, fare, cabin, embarked):
+        p_id = p_id
+        survival = survival
+        p_class = p_class
+        name = name
+        sex = sex
+        age = age
+        n_sib_sp = n_sib_sp
+        n_pa_ch = n_pa_ch
+        ticket = ticket
+        fare = fare
+        cabin = cabin
+        embarked = embarked
