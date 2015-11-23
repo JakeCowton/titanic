927ec64c6bc75f68ec177fdd35136c9f447c68fc
Jake Cowton <jake.cowton@newcastle.ac.uk>
2015-11-23 16:45:57 +0000

    Basic random forrest implementation

---
+++ src/rand_forrest.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import csv
+from sklearn.ensemble import RandomForestClassifier
+from utils import write_results
+
+train_df = pd.read_csv("../data/train.csv", header=0)
+test_df = pd.read_csv("../data/test.csv", header=0)
+
+train_df = train_df.drop(["PassengerId",
+                          "Name", "Sex", "Age", "SibSp", "Parch",
+                          "Ticket", "Fare", "Cabin", "Embarked"],
+                          axis=1)
+
+test_ids = test_df.PassengerId.values
+test_df = test_df.drop(["PassengerId",
+                        "Name", "Sex", "Age", "SibSp", "Parch",
+                        "Ticket", "Fare", "Cabin", "Embarked"],
+                          axis=1)
+
+train_data = train_df.values
+test_data = test_df.values
+
+forest = RandomForestClassifier(n_estimators=100,
+                                criterion="entropy",
+                                verbose=9)
+forest = forest.fit(train_data[0::,1::], train_data[0::,0])
+
+output = forest.predict(test_data).astype(int)
+
+write_results("rand_forrest.csv", test_ids, output)
---
+++ src/utils.py
@@ -0,0 +1,7 @@
+def write_results(filename, ids, output):
+    with open(filename, "wb") as f:
+        f.write("PassengerId, Survived\n")
+        for i in range(len(output)):
+            f.write("%d, %d\n" % (ids[i], output[i]))
+
+    return True
