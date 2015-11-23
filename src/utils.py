2380b34e430b6cfafe3475ad27b26754cebe6480
Jake Cowton <jake.cowton@newcastle.ac.uk>
2015-11-23 16:48:35 +0000

    Removed space

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 def write_results(filename, ids, output):
     with open(filename, "wb") as f:
-        f.write("PassengerId, Survived\n")
+        f.write("PassengerId,Survived\n")
         for i in range(len(output)):
             f.write("%d, %d\n" % (ids[i], output[i]))
 
