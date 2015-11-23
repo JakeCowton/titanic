fb15967c796f3a19392afd93c2413d4036d39404
Jake Cowton <jake.cowton@newcastle.ac.uk>
2015-11-23 20:11:22 +0000

    Created training and eval data

--- src/classifiers.py
+++ src/classifiers.py
@@ -2,20 +2,32 @@ import pandas as pd
 import numpy as np
 import csv
 from sklearn.ensemble import RandomForestClassifier
-from utils import write_results, read_data
+from utils import write_results, read_data, get_training_data,\
+                  get_evaluation_data, get_testing_data
 
 
 def random_forest():
     """
     A random forest classifier
     """
+
     print "Random Forest Classifier"
 
-    train_df, test_df = read_data()
+    training_data = get_training_data()
+    eval_data = get_evaluation_data()
+    testing_data = get_testing_data()
 
     ids = test_df.PassengerId.values
 
-    train_df, test_df = get_pclass_data()
+    # train_df = train_df.drop(["PassengerId",
+    #                           "Name", "Sex", "Age", "SibSp", "Parch",
+    #                           "Ticket", "Fare", "Cabin", "Embarked"],
+    #                           axis=1)
+
+    # test_df = test_df.drop(["PassengerId",
+    #                         "Name", "Sex", "Age", "SibSp", "Parch",
+    #                         "Ticket", "Fare", "Cabin", "Embarked"],
+    #                           axis=1)
 
     train_data = train_df.values
     test_data = test_df.values
@@ -33,14 +45,27 @@ def random_forest():
 
     print "Done!"
 
+    return True
+
 def bp_nn():
     """
     A back-propagating neural network
     """
+
     print "Back-propagating neural network"
 
-    train_df, test_df = read_data()
+    training_data = get_training_data()
+    eval_data = get_evaluation_data()
+    testing_data = get_testing_data()
 
     ids = test_df.PassengerId.values
 
-    train_df, test_df = get_pclass_data()
+    # train_df = train_df.drop(["PassengerId",
+    #                           "Name", "Sex", "Age", "SibSp", "Parch",
+    #                           "Ticket", "Fare", "Cabin", "Embarked"],
+    #                           axis=1)
+
+    # test_df = test_df.drop(["PassengerId",
+    #                         "Name", "Sex", "Age", "SibSp", "Parch",
+    #                         "Ticket", "Fare", "Cabin", "Embarked"],
+    #                           axis=1)
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ utils.py
 
 Utility functions for titanic data analysis
 """
+import pandas as pd
+
 
 def write_results(filename, ids, output):
     root_path = "/home/jake/src/ncl/titanic/outputs/"
@@ -13,21 +15,14 @@ def write_results(filename, ids, output):
 
     return True
 
-def read_data():
+def get_training_data():
     train_df = pd.read_csv("../data/train.csv", header=0)
-    test_df = pd.read_csv("../data/test.csv", header=0)
+    return train_df.values[0:89]
 
-    return train_df, test_df
+def get_evaluation_data():
+    train_df = pd.read_csv("../data/train.csv", header=0)
+    return train_df.values[90:]
 
-def get_pclass_data(train_df, test_df):
-    train_df = train_df.drop(["PassengerId",
-                              "Name", "Sex", "Age", "SibSp", "Parch",
-                              "Ticket", "Fare", "Cabin", "Embarked"],
-                              axis=1)
-
-    test_df = test_df.drop(["PassengerId",
-                            "Name", "Sex", "Age", "SibSp", "Parch",
-                            "Ticket", "Fare", "Cabin", "Embarked"],
-                              axis=1)
-
-    return train_df, test_ids, test_df
+def get_testing_data():
+    test_df = pd.read_csv("../data/test.csv", header=0).values
+    return test_df.values
