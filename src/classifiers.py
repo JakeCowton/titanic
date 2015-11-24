adf100d2515c01f5ded5615c3ca907000aae32ae
Jake Cowton <jake.cowton@newcastle.ac.uk>
2015-11-24 11:32:28 +0000

    Shit got changed and now I have code

--- src/classifiers.py
+++ src/classifiers.py
@@ -1,9 +1,7 @@
-import pandas as pd
-import numpy as np
-import csv
 from sklearn.ensemble import RandomForestClassifier
-from utils import write_results, read_data, get_training_data,\
-                  get_evaluation_data, get_testing_data
+from utils import write_results, get_training_data,\
+                  get_evaluation_data, get_testing_data,\
+                  calculate_accuracy
 
 
 def random_forest():
@@ -13,37 +11,49 @@ def random_forest():
 
     print "Random Forest Classifier"
 
-    training_data = get_training_data()
-    eval_data = get_evaluation_data()
-    testing_data = get_testing_data()
+    train_df = get_training_data()
+    eval_df = get_evaluation_data()
+    test_df = get_testing_data()
 
     ids = test_df.PassengerId.values
 
-    # train_df = train_df.drop(["PassengerId",
-    #                           "Name", "Sex", "Age", "SibSp", "Parch",
-    #                           "Ticket", "Fare", "Cabin", "Embarked"],
-    #                           axis=1)
+    train_df = train_df.drop(["PassengerId",
+                              "Name", "Sex", "Age", "SibSp", "Parch",
+                              "Ticket", "Fare", "Cabin", "Embarked"],
+                              axis=1)
 
-    # test_df = test_df.drop(["PassengerId",
-    #                         "Name", "Sex", "Age", "SibSp", "Parch",
-    #                         "Ticket", "Fare", "Cabin", "Embarked"],
-    #                           axis=1)
+    eval_df = eval_df.drop(["PassengerId", "Survived",
+                            "Name", "Sex", "Age", "SibSp", "Parch",
+                            "Ticket", "Fare", "Cabin", "Embarked"],
+                            axis=1)
+
+    test_df = test_df.drop(["PassengerId",
+                             "Name", "Sex", "Age", "SibSp", "Parch",
+                             "Ticket", "Fare", "Cabin", "Embarked"],
+                             axis=1)
 
     train_data = train_df.values
+    eval_data = eval_df.values
     test_data = test_df.values
 
-    forest = RandomForestClassifier(n_estimators=100,
+    forest = RandomForestClassifier(n_estimators=10000,
                                     criterion="entropy")
     print "Training..."
-    forest = forest.fit(train_data[0::,1::], train_data[0::,0])
+    inputs = train_data[0::,1::]
+    expected_outputs = train_data[0::,0]
+    forest = forest.fit(inputs, expected_outputs)
 
     print "Predicting..."
-    output = forest.predict(test_data).astype(int)
+    evaluation = forest.predict(eval_data)
+
+    print "Accuracy: {:10.4f}".format(calculate_accuracy(evaluation))
+
+    output = forest.predict(test_data)
 
     print "Writing results..."
     write_results("rand_forest.csv", ids, output)
 
-    print "Done!"
+    print "--- Done ---"
 
     return True
 
@@ -60,12 +70,3 @@ def bp_nn():
 
     ids = test_df.PassengerId.values
 
-    # train_df = train_df.drop(["PassengerId",
-    #                           "Name", "Sex", "Age", "SibSp", "Parch",
-    #                           "Ticket", "Fare", "Cabin", "Embarked"],
-    #                           axis=1)
-
-    # test_df = test_df.drop(["PassengerId",
-    #                         "Name", "Sex", "Age", "SibSp", "Parch",
-    #                         "Ticket", "Fare", "Cabin", "Embarked"],
-    #                           axis=1)
--- src/utils.py
+++ src/utils.py
@@ -16,13 +16,46 @@ def write_results(filename, ids, output):
     return True
 
 def get_training_data():
+    """
+    Gets first 800 rows of train.csv
+    """
     train_df = pd.read_csv("../data/train.csv", header=0)
-    return train_df.values[0:89]
+    return train_df[0:800]
 
 def get_evaluation_data():
+    """
+    Gets last 90 rows of train.csv for accuracy calculation
+    """
     train_df = pd.read_csv("../data/train.csv", header=0)
-    return train_df.values[90:]
+    return train_df[801:]
 
 def get_testing_data():
-    test_df = pd.read_csv("../data/test.csv", header=0).values
-    return test_df.values
+    """
+    Gets the testing data
+    """
+    test_df = pd.read_csv("../data/test.csv", header=0)
+    return test_df
+
+def calculate_accuracy(output):
+    """
+    output[x][0] = PassengerId
+    output[x][1] = Survived
+    Should be 90 in length
+    """
+    if len(output) != 90:
+        raise IndexError("Output should be 90, not %d" % len(output))
+
+    eval_df = get_evaluation_data()
+    eval_df = eval_df.drop(["PassengerId", "Pclass",
+                  "Name", "Sex", "Age", "SibSp", "Parch",
+                  "Ticket", "Fare", "Cabin", "Embarked"],
+                  axis=1)
+    eval_data = eval_df.values
+
+    correct = 0
+
+    for i in range(len(output)):
+        if output[i] == eval_data[i][0]:
+            correct += 1
+
+    return correct / 90.0 # Number of eval samples
